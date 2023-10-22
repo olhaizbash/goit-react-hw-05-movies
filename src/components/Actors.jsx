85c3c872cc85046be7e47e0e7523a2e375daf628
Bohdan Izbash <olgaizbash@gmail.com>
2023-10-22 15:27:19 +0300

    second

--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import css from './Actors.module.css';
 
 export const Actors = ({ name, profile_path, character }) => {
-  const BASE_IMG = 'https://image.tmdb.org/t/p/w500';
+  const BASE_IMG = 'https://image.tmdb.org/t/p/w500/';
+  const defaultImg =
+    'https://media.istockphoto.com/id/1396814518/pl/wektor/obraz-ju%C5%BC-wkr%C3%B3tce-brak-zdj%C4%99cia-brak-miniatury-ilustracja-wektorowa.jpg?s=2048x2048&w=is&k=20&c=bUnbketbTn26iM9fYWY5rgDy5BoQrsToBgHUF2SrePA=';
   return (
     <div>
-      <img className={css.img} src={`${BASE_IMG}${profile_path}`} alt={name} />
+      <img
+        className={css.img}
+        src={profile_path ? `${BASE_IMG}${profile_path}` : defaultImg}
+        alt={name}
+      />
       <p>{name}</p>
       <p>Character: {character}</p>
     </div>
--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -4,14 +4,16 @@ import css from './MovieItem.module.css';
 
 export const MovieItem = ({ id, poster_path, title }) => {
   const location = useLocation();
-  const BASE_IMG = 'https://image.tmdb.org/t/p/w500';
+  const BASE_IMG = 'https://image.tmdb.org/t/p/w500/';
+  const defaultImg =
+    'https://media.istockphoto.com/id/1396814518/pl/wektor/obraz-ju%C5%BC-wkr%C3%B3tce-brak-zdj%C4%99cia-brak-miniatury-ilustracja-wektorowa.jpg?s=2048x2048&w=is&k=20&c=bUnbketbTn26iM9fYWY5rgDy5BoQrsToBgHUF2SrePA=';
 
   return (
     <li className={css.movieitem}>
       <Link to={`/movies/${id}`} state={{ from: location }}>
         <img
           className={css.movieimg}
-          src={`${BASE_IMG}${poster_path}`}
+          src={poster_path ? `${BASE_IMG}${poster_path}` : defaultImg}
           alt={title}
         />
         <p className={css.movietitle}>{title}</p>
